b47c42b996d12581f8c580935d1d1aae1669c692
WangXi <jason942371@gmail.com>
2023-07-22 15:25:01 +0800

    Refactor some .scss to .jsx type with styled lib

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,22 @@
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
+
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <DirectoryItemContainer>
+      {/* to={`/shop/${title.toLowerCase()}`} */}
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
+
+export default DirectoryItem;
