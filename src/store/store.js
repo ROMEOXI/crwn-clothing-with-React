a4c244549e6e95f6561880647191033f16f5615f
WangXi <jason942371@gmail.com>
2024-06-18 19:20:48 +0800

    Migrate custom logger to logger.js.

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) return next(action);
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currnetState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -30,9 +18,11 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST'],
+        ignoredActions: ["persist/PERSIST"],
       },
-    }).concat(loggerMiddleware);
+    })
+      .concat([process.env.NODE_ENV === "development" && logger])
+      .filter(Boolean);
   },
 });
 
