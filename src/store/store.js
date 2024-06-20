60710c39f518bde7b0f3b03ca82a43a490aaf6f4
WangXi <jason942371@gmail.com>
2024-06-20 19:58:37 +0800

    Add redux-thunk and make Sppiner component

--- src/store/store.js
+++ src/store/store.js
@@ -2,16 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -20,9 +26,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: ["persist/PERSIST"],
       },
-    })
-      .concat([process.env.NODE_ENV === "development" && logger])
-      .filter(Boolean);
+    }).concat(middleWares);
   },
 });
 
